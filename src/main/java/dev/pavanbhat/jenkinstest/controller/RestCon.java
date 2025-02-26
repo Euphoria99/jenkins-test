67963664348cd7e1293d20ba73745f9bdfb8defb
Euphoria99 <363pavanbhat@gmail.com>
2025-02-26 00:32:51 +0000

    init files

---
+++ src/main/java/dev/pavanbhat/jenkinstest/controller/RestCon.java
@@ -0,0 +1,17 @@
+package dev.pavanbhat.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkinstest")
+public class RestCon {
+
+
+    @GetMapping("/sayhello")
+    public String sayHello(){
+        return "The end point works as expected";
+    }
+
+}
